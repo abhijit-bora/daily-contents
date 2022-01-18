9ccf6be0a7b73eda1af321da6598a682ffd93107
abhijit-bora <50410295+abhijit-bora@users.noreply.github.com>
2022-01-18 18:47:26 +0530

    login page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Route, Switch, BrowserRouter, Redirect } from "react-router-dom";
+import LogInPage from "./pages/LogInPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={LogInPage} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/LogInPage.js
@@ -0,0 +1,95 @@
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import {
+  Button,
+  Form,
+  Grid,
+  Header,
+  Image,
+  Message,
+  Segment,
+} from "semantic-ui-react";
+// import axios from "axios";
+import "../App.css";
+
+const LogInPage = ({ setIsLoggedIn }) => {
+  const [formDetails, setFormDetails] = useState({ emailId: "", password: "" });
+  const history = useHistory();
+  async function submitForm() {
+    console.log(formDetails);
+    history.push("/");
+  }
+  //   try {
+  //     console.log(formDetails);
+  //     await axios
+  //       .post("http://localhost:3005/signin", formDetails)
+  //       .then((res) => {
+  //         console.log(res);
+
+  //         localStorage.setItem("token", res.data.token);
+  //         localStorage.setItem("email", res.data.user_.emailId);
+  //         window.location.href = "http://localhost:3000/";
+  //       })
+  //       .catch((err) => {
+  //         localStorage.removeItem("todo-app-token");
+  //         localStorage.removeItem("todo-app-email");
+  //       });
+  //   } catch (err) {}
+  // }
+
+  return (
+    <>
+      <Header
+        as="h1"
+        color="teal"
+        textAlign="center"
+        style={{ padding: "2vh" }}
+      >
+        DAILY CONTENT
+      </Header>
+      <Grid
+        textAlign="center"
+        style={{ height: "100vh" }}
+        verticalAlign="middle"
+      >
+        <Grid.Column style={{ maxWidth: 450 }}>
+          <Header as="h2" color="teal" textAlign="center">
+            Log-in to your account
+          </Header>
+          <Form size="large">
+            <Segment stacked>
+              <Form.Input
+                fluid
+                icon="user"
+                iconPosition="left"
+                placeholder="E-mail address"
+                onChange={(e) =>
+                  setFormDetails({ ...formDetails, emailId: e.target.value })
+                }
+              />
+              <Form.Input
+                fluid
+                icon="lock"
+                iconPosition="left"
+                placeholder="Password"
+                type="password"
+                onChange={(e) =>
+                  setFormDetails({ ...formDetails, password: e.target.value })
+                }
+              />
+
+              <Button color="teal" fluid size="large" onClick={submitForm}>
+                Login
+              </Button>
+            </Segment>
+          </Form>
+          <Message>
+            New to us? <a href="#">Sign Up</a>
+          </Message>
+        </Grid.Column>
+      </Grid>
+    </>
+  );
+};
+
+export default LogInPage;
