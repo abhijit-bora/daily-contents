d5eb12b21738e28acdbee36d63fb5e4cf9600e83
abhijit-bora <50410295+abhijit-bora@users.noreply.github.com>
2022-01-20 17:24:35 +0530

    added new page

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -6,9 +6,10 @@ import {
   Header,
   Image,
   Modal,
-  Segment,
+  List,
   Button,
 } from "semantic-ui-react";
+import { Player } from "video-react";
 
 import "../../App.css";
 import "./Slider.css";
@@ -17,7 +18,8 @@ import BtnSlider from "./BtnSlider";
 import data from "../../data/data";
 
 export default function Slider() {
-  let day = 1;
+  let day = 5;
+  let date = 1;
   const [slideIndex, setSlideIndex] = useState(1);
 
   const nextSlide = () => {
@@ -48,26 +50,25 @@ export default function Slider() {
             <Image src={data.chakra[day].image} className="cardbody" />
             <Card.Content textAlign="center">
               <Card.Header>{data.chakra[day].title}</Card.Header>
-              <Card.Description>{data.chakra[day].des}</Card.Description>
+            </Card.Content>
+          </Card>
+        </div>
+        <div className={slideIndex === 2 ? "slide active-anim" : "slide"}>
+          <Card className="cardbody" fluid="1">
+            {/* <Image src={data.chakra[day].image} className="cardbody" /> */}
+            <Card.Content textAlign="center">
+              <Card.Header>Affirmations:</Card.Header>
+              <Card.Description>
+                <List as="ul">
+                  {data.chakra[day].des.map((obj, i) => {
+                    return <List.Item as="li">{obj}</List.Item>;
+                  })}
+                </List>
+              </Card.Description>
             </Card.Content>
           </Card>
         </div>
         {/* <div
-                className={
-                  slideIndex === 2
-                    ? "slide active-anim"
-                    : "slide"
-                }
-              >
-                <Card className="cardbody" fluid="1">
-                  <Image src={im} className="cardbody" />
-                  <Card.Content textAlign="center">
-                    <Card.Header>{t}</Card.Header>
-                    <Card.Description>{obj}</Card.Description>
-                  </Card.Content>
-                </Card>
-              </div>
-              <div
                 className={
                   slideIndex === 3
                     ? "slide active-anim"
@@ -82,21 +83,16 @@ export default function Slider() {
                   </Card.Content>
                 </Card>
               </div> */}
-        {/* <div
-                className={
-                  slideIndex === 4
-                    ? "slide active-anim"
-                    : "slide"
-                }
-              >
-                <Card className="cardbody" fluid="1">
-                  <Image src={im} className="cardbody" />
-                  <Card.Content textAlign="center">
-                    <Card.Header>{t}</Card.Header>
-                    <Card.Description>{obj}</Card.Description>
-                  </Card.Content>
-                </Card>
-              </div> */}
+
+        <div className={slideIndex === 3 ? "slide active-anim" : "slide"}>
+          <Card className="cardbody" fluid="1">
+            <Player src="videos/9.mp4" className="cardbody" />
+
+            <Card.Content textAlign="center">
+              <Card.Header>Daily Concentration</Card.Header>
+            </Card.Content>
+          </Card>
+        </div>
 
         <BtnSlider moveSlide={nextSlide} direction={"next"} />
         <BtnSlider moveSlide={prevSlide} direction={"prev"} />
--- src/data/data.js
+++ src/data/data.js
@@ -5,49 +5,85 @@ export default {
       image:
         "https://img.playbuzz.com/image/upload/q_auto:best,f_auto,fl_lossy,w_640,c_limit,dpr_1/v1596363098/hpdszx5chxz3irbuukim.jpg",
       title: "Root Chakra Affirmations",
-      des: "",
+      des: [
+        "I am centered and grounded.",
+        "I am powerful, rooted, and strong.",
+        "I am stable and secure.",
+        "I am financially secure.",
+        "The universe will always provide for me.",
+      ],
     },
     {
       day: "monday",
       image:
         "https://img.playbuzz.com/image/upload/q_auto:best,f_auto,fl_lossy,w_640,c_limit,dpr_1/v1596363258/jklfmdgnfmpkx2o62sdw.jpg",
       title: "Sacral Chakra Affirmations",
-      des: "",
+      des: [
+        "The sweetness of life flows through me, and I radiate its joy.",
+        "I feel safe to express my sexual self in fun, creative, and healthy ways.",
+        "I flow with inspiration and creativity.",
+        "My emotions are free flowing and balanced.",
+        "I embrace pleasure and abundance.",
+      ],
     },
     {
       day: "tuesday",
       image:
         "https://img.playbuzz.com/image/upload/q_auto:best,f_auto,fl_lossy,w_640,c_limit,dpr_1/v1596363369/odnwqti2mde60bs5rm5w.jpg",
       title: "Solar Plexus Chakra Affirmations",
-      des: "",
+      des: [
+        "I stand in my personal power. ",
+        "I make decisions with confidence and conviction.",
+        "The only thing I need to control is how I respond to situations.",
+        "I have the courage to create positive change in my life.",
+        "I feel motivated to pursue my purpose.",
+      ],
     },
     {
       day: "wednesday",
       image:
         "https://img.playbuzz.com/image/upload/q_auto:best,f_auto,fl_lossy,w_640,c_limit,dpr_1/v1596364602/juxoluo0eks4suuud7yi.jpg",
       title: "Heart Chakra",
-      des: "",
+      des: ["", "", "", "", ""],
     },
     {
       day: "thursday",
       image:
         "https://img.playbuzz.com/image/upload/q_auto:best,f_auto,fl_lossy,w_640,c_limit,dpr_1/v1596364492/wqmf8puwznjklkxqu2jb.jpg",
       title: "Throat Chakra",
-      des: "",
+      des: [
+        "I welcome love with an open heart. ",
+        "My heart is healed from all the wounds of the past.",
+        "I naturally attract love everywhere I go.",
+        "I release and let go of all resentment.",
+        "My heart space radiates powerful green light.",
+      ],
     },
     {
       day: "friday",
       image:
         "https://img.playbuzz.com/image/upload/q_auto:best,f_auto,fl_lossy,w_640,c_limit,dpr_1/v1596364226/nszw1l8dfqdudkzydkxd.jpg",
       title: "Ajna Chakra",
-      des: "",
+      des: [
+        "I communicate confidently and with ease.",
+        "I feel comfortable speaking my mind.",
+        "I am an active listener.",
+        "I express myself honestly with clear intent.",
+        "I know when it is time to listen.",
+      ],
     },
     {
       day: "saturday",
       image:
         "https://img.playbuzz.com/image/upload/q_auto:best,f_auto,fl_lossy,w_640,c_limit,dpr_1/v1596364333/wkyoxkkcpbbdtu0y8vup.jpg",
       title: "Crown Chakra",
-      des: "",
+      des: [
+        "I trust my intuition.",
+        "I feel connected to my spiritual truth.",
+        "I let my inner wisdom guide me.",
+        "I trust my decision.",
+        "I quiet my mind with ease.",
+      ],
     },
   ],
 };
