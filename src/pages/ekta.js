71dcf720b7e21267b9e604536063a8c3d6f5ae82
abhijit-bora <50410295+abhijit-bora@users.noreply.github.com>
2022-01-23 23:48:11 +0530

    add

--- src/pages/ekta.js
+++ src/pages/ekta.js
@@ -73,3 +73,9 @@ const LogInPage = ({ setIsLoggedIn }) => {
 };
 
 export default LogInPage;
+
+<link
+  async
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/semantic-ui@2/dist/semantic.min.css"
+/>;
